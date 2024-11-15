0d1e3033b459495cc321cb3e5fd1ff4fc64f27f3
VolleyLord <artembuinov05@gmail.com>
2024-11-15 11:38:16 +0300

    fix: set up middleware for correct work with meetups; set up correct jwt_secret keys

--- auth-service/src/middleware/authMiddleware.js
+++ auth-service/src/middleware/authMiddleware.js
@@ -3,14 +3,15 @@ const { JWT_SECRET } = process.env;
 
 export default (req, res, next) => {
     // Получаем токен из заголовка Authorization с префиксом Bearer
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : null;
+    //const authHeader = req.headers['authorization'];
+    //const token = authHeader && authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : null;
 
     // Проверяем токен в cookie, если он не найден в заголовке
     const tokenFromCookie = req.cookies && req.cookies.token;
 
     // Выбираем токен из заголовка, либо из cookie
-    const finalToken = token || tokenFromCookie;
+    //const finalToken = token || tokenFromCookie;
+    const finalToken = tokenFromCookie;
 
     if (!finalToken) {
         return res.status(403).json({ message: 'Token required' });
